bfdc60ddeb7a1c4e688cea6cb1fa6bf55e6f1720
Brooke <46959407+nimajnebec@users.noreply.github.com>
2026-04-20 18:43:40 +0100

    feat: honourary members

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,6 +1,13 @@
-declare module "@xivapi/nodestone" {
-	import { Request } from "express";
+interface ImportMetaEnv {
+	readonly FREECOMPANY_ID: string;
+	readonly HONOURARY_IDS: string;
+}
 
+interface ImportMeta {
+	readonly env: ImportMetaEnv;
+}
+
+declare module "@xivapi/nodestone" {
 	export declare abstract class PageParser<P, T> {
 		parse(req: { params: P }, columnsPrefix?: string): Promise<T>;
 	}
@@ -29,5 +36,46 @@ declare module "@xivapi/nodestone" {
 		RankIcon: string;
 		World: string;
 		DC: string;
+	}
+
+	export declare class Character extends PageParser<{ characterId: string }, CharacterParseResult> {}
+
+	export interface CharacterParseResult {
+		Name: string;
+		Avatar: string;
+		Level: number;
+		Bio: string;
+		Rank: string;
+		Nameday: string;
+		Portrait: string;
+		Race: string;
+		Tribe: string;
+		Gender: string;
+		World: string;
+		DC: string;
+		Strength: number;
+		Dexterity: number;
+		Vitality: number;
+		Intelligence: number;
+		Mind: number;
+		CriticalHitRate: number;
+		Determination: number;
+		DirectHitRate: number;
+		Defense: number;
+		MagicDefense: number;
+		AttackPower: number;
+		SkillSpeed: number;
+		AttackMagicPotency: number;
+		HealingMagicPotency: number;
+		SpellSpeed: number;
+		Tenacity: number;
+		Piety: number;
+		Hp: number;
+		MpGpCp: number;
+		MpGpCpParameterName: string;
+		FreeCompany: {
+			ID: string;
+			Name: string;
+		} | null;
 	}
 }
--- src/xivapi.ts
+++ src/xivapi.ts
@@ -1,18 +1,45 @@
-import { FCMembers, type FCMembersParseResult, type PaginatedParseResult } from "@xivapi/nodestone";
+import { Character, FCMembers, type FCMembersParseResult, type PaginatedParseResult } from "@xivapi/nodestone";
 
-const CACHE_DURATION = 10 * 60 * 1000; // 10 minutes
-const FC_ID = "9235053248388344200";
+const CACHE_DURATION = 6 * 60 * 60 * 1000; // 6 hours
+const FREECOMPANY = import.meta.env.FREECOMPANY_ID;
+const HONOURARAY = import.meta.env.HONOURARY_IDS.split(",");
 
-let cache: { data: PaginatedParseResult<FCMembersParseResult> | null; timestamp: number } = {
+interface FcMember {
+	FcRankIcon: string;
+	Avatar: string;
+	FcRank: string;
+	Name: string;
+	ID: number;
+}
+
+let cache: { data: FcMember[] | null; timestamp: number } = {
 	data: null,
 	timestamp: 0,
 };
 
-export async function getMembers() {
+export async function getMembers(): Promise<FcMember[]> {
 	if (cache.data && Date.now() - cache.timestamp < CACHE_DURATION) return cache.data;
 	else {
-		const data = await new FCMembers().parse({ params: { fcId: FC_ID } });
+		const tasks = await Promise.all([
+			new FCMembers().parse({ params: { fcId: FREECOMPANY } }).then((r) => r.List),
+			...HONOURARAY.map(getHonourary),
+		]);
+
+		const data = tasks.flat().filter((m) => m !== null);
 		cache = { data, timestamp: Date.now() };
 		return data;
 	}
+}
+
+async function getHonourary(id: string): Promise<FcMember | null> {
+	const result = await new Character().parse({ params: { characterId: id } });
+
+	if (result.FreeCompany?.ID === FREECOMPANY) return null;
+
+	return {
+		...result,
+		FcRank: "Honourary",
+		FcRankIcon: "/honourary.png",
+		ID: parseInt(id),
+	};
 }
